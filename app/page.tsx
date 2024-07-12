344b1ef79e99bc4d15d546595dbde0683e0ec2d8
Andrés Olivé <andresolive@gmail.com>
2024-07-12 13:47:56 -0600

    create server component example

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
+const getData = async () => {
+  await new Promise((res) => setTimeout(() => res(0), 2000));
+  return { data: [1, 2, 3] };
+};
+
+const Home = async () => {
+  const data = await getData();
+  console.log(data);
+
   return <div>Home</div>;
-}
+};
+
+export default Home;
