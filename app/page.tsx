0339bdf66fa9fadaf7d00ebab91a5955a7f2a1a0
Andrés Olivé <andresolive@gmail.com>
2024-07-12 16:07:42 -0600

    display NewTodoForm

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import NewTodoForm from '@/components/NewTodoForm';
+
 const getData = async () => {
   await new Promise((res) => setTimeout(() => res(0), 2000));
   return { data: [1, 2, 3] };
@@ -7,7 +9,7 @@ const Home = async () => {
   const data = await getData();
   console.log(data);
 
-  return <div>Home</div>;
+  return <NewTodoForm />;
 };
 
 export default Home;
