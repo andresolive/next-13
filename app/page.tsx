d3564fb37a93c02c904636148cbb449fc840f753
Andrés Olivé <andresolive@gmail.com>
2024-07-15 10:29:17 -0600

    fix home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
-import NewTodoForm from '@/app/components/NewTodoForm';
-
-const getData = async () => {
-  await new Promise((res) => setTimeout(() => res(0), 2000));
-  return { data: [1, 2, 3] };
-};
-
 const Home = async () => {
-  const data = await getData();
-  console.log(data);
-
-  return <NewTodoForm />;
+  return <div>Home</div>;
 };
 
 export default Home;
