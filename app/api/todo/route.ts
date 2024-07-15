d07f632a1524a8420186715df3333a1e7b5d8a77
Andrés Olivé <andresolive@gmail.com>
2024-07-15 13:06:20 -0600

    create api route

---
+++ app/api/todo/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+
+export const GET = async (request: Request, response: Response) => {
+  return NextResponse.json({ message: 'hello' });
+};
+
+export const POST = async (request: Request, response: Response) => {
+  const data = await request.json();
+  return NextResponse.json({ message: data });
+};
