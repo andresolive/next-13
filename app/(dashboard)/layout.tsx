79144956112e41fd1f121f100e3224b94e6452c8
Andrés Olivé <andresolive@gmail.com>
2024-07-15 10:29:35 -0600

    add props

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,16 @@
-const DashboardLayout = ({ children }) => {
+import NewTodoForm from '../components/NewTodoForm';
+
+interface IDashboardLayoutProps {
+  children: any;
+}
+
+const DashboardLayout = ({ children }: IDashboardLayoutProps) => {
   return (
     <div>
       <h1>Dashboard</h1>
+      <div>
+        <NewTodoForm />
+      </div>
       <div>{children}</div>
     </div>
   );
