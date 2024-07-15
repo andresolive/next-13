120bde6c1efb3a1517d31f6deb73a06d6fe34a8a
Andrés Olivé <andresolive@gmail.com>
2024-07-15 12:45:57 -0600

    create completeTodo action

--- utils/actions.ts
+++ utils/actions.ts
@@ -2,6 +2,15 @@
 import { revalidatePath } from 'next/cache';
 import db from './db';
 
+export const completeTodo = async (id: any) => {
+  await db.todo.update({
+    where: { id },
+    data: { completed: true },
+  });
+
+  revalidatePath('/todos');
+};
+
 export const newTodo = async (formData: any) => {
   const todo = await db.todo.create({
     data: {
