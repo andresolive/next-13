9619295b34c7247b234fd9c3f2fb1ff4d2703b82
Andrés Olivé <andresolive@gmail.com>
2024-07-15 12:46:15 -0600

    add complete functionality

--- app/components/Todo.tsx
+++ app/components/Todo.tsx
@@ -1,9 +1,25 @@
+'use client';
+
+import { useTransition } from 'react';
+import { completeTodo } from '@/utils/actions';
+
 interface ITodoProps {
   todo: any;
 }
 
 const Todo = ({ todo }: ITodoProps) => {
-  return <div>{todo.content}</div>;
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <div
+      className={`border border-black/20 cursor-pointer ${
+        todo.completed ? 'line-through text-gray-900' : ''
+      }`}
+      onClick={() => startTransition(() => completeTodo(todo.id))}
+    >
+      {todo.content}
+    </div>
+  );
 };
 
 export default Todo;
